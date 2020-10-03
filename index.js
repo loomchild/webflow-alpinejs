7e3bcb54be767f6cc2ba6ea042de5233dec5ec85
Jarek Lipski <loomchild@loomchild.net>
2020-10-03 20:33:22 +0200

    add js

---
+++ index.js
@@ -0,0 +1,45 @@
+function replaceByTemplate(el) {
+  const opening = new RegExp('^<' + el.tagName, 'i')
+	const closing = new RegExp('</' + el.tagName + '>$', 'i')
+
+	const newOuterHTML = el.outerHTML
+  	.replace(opening, '<template')
+    .replace(closing, '</template>')
+
+  el.outerHTML = newOuterHTML
+}
+
+function replaceDotAttributes(el) {
+  const alpineAttributes = []
+  for (let i = 0; i < el.attributes.length; ++i) {
+    const a = el.attributes[i]
+    if (a.name.startsWith('x-')) {
+      alpineAttributes.push(a)
+    }
+  }
+  
+  alpineAttributes.forEach(a => {
+    const m = a.name.match(/^(x-[^:]+)(:.+)$/)
+    if (m) {
+      let newA = null
+      if (['x-bind', 'x-on'].includes(m[1])) {
+        const suffix = m[2].substring(1)
+        if (suffix.includes(':')) {
+          newA = m[1] + ':' + suffix.replace(/:/g, '.')
+        }
+      } else {
+        newA = m[1] + m[2].replace(/:/g, '.')
+      }
+      if (newA) {
+        el.setAttribute(newA, a.value)
+        el.removeAttribute(a.name)
+      }
+    }
+  })
+}
+
+document.querySelectorAll('[x-data] *').forEach((el) => {
+  replaceDotAttributes(el)
+})
+
+document.querySelectorAll('[x-data] [x-for], [x-data] [x-if]').forEach(replaceByTemplate)
