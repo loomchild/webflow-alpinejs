adab701c894a16317dd46f541637cad778b60b71
Jarek Lipski <loomchild@loomchild.net>
2021-09-03 20:29:05 +0200

    remove dummy

--- init.js
+++ init.js
@@ -49,7 +49,7 @@ function removeUnnecessaryAttributeValues (el) {
   const attributes = getAlpineAttributes(el)
 
   attributes.forEach(a => {
-    if (a.name.match(/^x-transition.*(?!(enter|leave|blaaa))/)) {
+    if (a.name.match(/^x-transition.*(?!(enter|leave))/)) {
       el.setAttribute(a.name, '')
     }
   })
